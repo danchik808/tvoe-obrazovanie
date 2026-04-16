d84053f04b9fb8fa85372744a67ccccf048b5de2
Daniil Lekoncev <daniil.l75@yandex.ru>
2026-04-16 22:01:46 +0300

    add

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,10 +1,11 @@
 import "./footer.css";
 
 export default function Footer() {
+    const currentYear = new Date().getFullYear();
     return (
         <div className="footer">
             <a href="" className="footer_item">Правила хранения данных</a>
-            <p className="footer_item">2026</p>
+            <p className="footer_item">{currentYear}</p>
             <a href="" className="footer_item">Правила сбора информации</a>
         </div>
     );
