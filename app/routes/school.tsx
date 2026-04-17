fd8e43d8fe5e3a935f2ef4fc26f25d8813cd4ae5
Daniil Lekoncev <daniil.l75@yandex.ru>
2026-04-17 18:59:04 +0300

    add

--- app/routes/school.tsx
+++ app/routes/school.tsx
@@ -1,37 +1,49 @@
 import { useParams } from "react-router";
+import Header from "../components/header";
 import { useEffect, useState } from "react";
 import { supabase } from "../lib/supabase";
-import type { Route } from "./+types/school";
-
-export function meta({ }: Route.MetaArgs) {
-  return [
-    { title: `` },
-  ];
-}
 
 interface SchoolFromSupabase {
-  id: number;
-  shortName: string;
-  district: string;
-  shortDescription: string;
-  slug: string;  
+    id: number;
+    shortName: string;
+    district: string;
+    shortDescription: string;
+    slug: string;
+    profiles: any[];
 }
 
 export default function SchoolPage() {
-  const { slug } = useParams();
-  const [school, setSchool] = useState<SchoolFromSupabase | null>(null);
+    const { slug } = useParams();
+    const [school, setSchool] = useState<SchoolFromSupabase | null>(null);
 
-  useEffect(() => {
-    supabase.from("schools").select("*").eq("slug", slug).single()
-      .then(({ data }) => setSchool(data));
-  }, [slug]);
-  if (!school) return <div>Загрузка...</div>;
+    useEffect(() => {
+        supabase.from("schools").select("*").eq("slug", slug).single()
+            .then(({ data }) => {
+                setSchool(data);
+                if (data) {
+                    document.title = data.fullName;
+                }
+            });
+    }, [slug]);
 
-  return (
-    <div>
-      <h1>{school.shortName}</h1>
-      <p>{school.shortDescription}</p>
-      <p>{school.district}</p>
-    </div>
-  );
+    if (!school) return <div>Загрузка...</div>;
+
+    return (
+        <>
+            <Header logoTheme='light' />
+            <div className="school-container">
+
+                <h1>{school.shortName}</h1>
+                <p>{school.shortDescription}</p>
+                <p>{school.district}</p>
+
+                {school.profiles && school.profiles.map((item, index) => (
+                    <div key={index}>
+                        <p>Год: {item.name}</p>
+                        <p>Рейтинг: {item.profiles}</p>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }
